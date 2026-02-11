5bdf6ad44f88de1554e57fd5af21d4118f9965ad
r-matsura-s <starkirikyoku@gmail.com>
2026-02-11 20:34:46 +0900

    sstar ez の仮譜面作成

--- project/Source/Screen.h
+++ project/Source/Screen.h
@@ -9,7 +9,7 @@ namespace Screen
 	static const int WIDTH = 1280;
 	static const int HEIGHT = 720;
 	static const BOOL WINDOW_MODE = TRUE;
-	static const char* WINDOW_NAME = "project";
-	static const float WINDOW_EXTEND = 1.0f;
+	static const char* WINDOW_NAME = "Beat Slime Slide";
+	static const float WINDOW_EXTEND = 1.3f;
 	static const float FRAME_RATE = 60.0f;
 };
